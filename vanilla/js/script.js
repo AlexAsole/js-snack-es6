74ec6f42e10f80d181b90639551178859ddd46aa
Alex Giorgio Asole <alexgiorgio.asole@gmail.com>
2021-01-19 11:36:01 +0100

    esercizio corretto

---
+++ vanilla/js/script.js
@@ -0,0 +1,16 @@
+// Creare un array di oggetti:
+// Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
+// Stampare a schermo la bici con peso minore
+
+var bici = [
+  {'nome': 'Qui', 'peso': 7},
+  {'nome': 'Quo', 'peso': 6.5 },
+  {'nome': 'Qua', 'peso': 8.5}
+];
+var min = bici[0];
+for (var x = 1; x < bici.length; x++){
+    if (bici[x].peso < min.peso){
+      min = bici[x]
+    }
+}
+console.log(min)
