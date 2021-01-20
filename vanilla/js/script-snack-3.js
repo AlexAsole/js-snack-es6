b656ebe69c541c6cf5ea2d7225b1bdedf27b98a9
Alex Giorgio Asole <alexgiorgio.asole@gmail.com>
2021-01-20 14:56:16 +0100

    correzione

--- vanilla/js/script-snack-3.js
+++ vanilla/js/script-snack-3.js
@@ -1,17 +1,17 @@
 // Si scriva una funzione che accetti tre argomenti, un array e due numeri (A più piccolo di B).
 // La funzione ritornerà un nuovo array con i valori che hanno la posizione compresa tra i due numeri
-function newArray() {
+function newArray(arr, min, max) {
   var newArray = [];
-  for (var x = 0; x < numArray.length; x++) {
-    if ((x >= a) && (x <= b)) {
-      newArray.push(numArray[x])
+  for (var x = 0; x < arr.length; x++) {
+    if ((x >= min) && (x <= max)) {
+      newArray.push(arr[x])
     }
   }
   return newArray;
 }
 
 var numArray = [1,2,3,4,5,6,7,8,9,10];
-var a = 2;
-var b = 9;
+var a = 5;
+var b = 8;
 
-console.log(newArray());
+console.log(newArray(numArray, a, b));
